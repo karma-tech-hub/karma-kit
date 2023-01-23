ae227d7a0ae894b9598004521fa236e01a317634
Hasan Ahani <67734902+hasan-ahani@users.noreply.github.com>
2023-01-23 11:25:55 +0330

    [FIX] Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,52 @@
+const withMT = require("@material-tailwind/html/utils/withMT");
 
-module.exports = {
+
+module.exports = withMT({
   content: [
-    './*.php',
-    './*.js',
-    './*.json',
+    './404.php',
+    './archive.php',
+    './category.php',
+    './tag.php',
+    './footer.php',
+    './functions.php',
+    './header.php',
+    './index.php',
+    './page.php',
+    './single.php',
     './tailwind.config.js',
     './core/**/*.php',
-    './widgets/**/*.php',
-    './widgets/**/*.scss',
-    './widgets/**/*.js',
     './vendor/**/*.php',
     './src/**/*.js|css|scss',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.5rem',
+        sm: '2rem',
+        lg: '3rem',
+        xl: '4rem',
+      },
+      screens: {
+        sm: '600px',
+        md: '780px',
+        lg: '984px',
+        xl: '1296px',
+        '2xl': '1296px',
+      },
+    },
     fontFamily: {
+      'sans': [ 'Inter', 'sans-serif'],
+      'lato': [ 'Lato', 'sans-serif'],
     },
     extend: {
+      colors:{
+        primary: '#3A77FF',
+        secondary: '#0FE3FC',
+        light: '#F2F6F9',
+        gray: '#BBBBBB'
+      }
     },
   },
   plugins: [],
-}
+})
